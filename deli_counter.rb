88e1d504427b5791d8fbc18071323a2b6ddc4933
Laika Deunert <laikadeunert@Laikas-Laptop.local>
2021-01-17 19:15:13 -0800

    commit tree 13d84e1ff7d0a6beb155918aa1ca9718fe09ef99

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,34 @@
-# Write your code here.
+
+#line method
+
+def line(katz_deli)
+    if katz_deli.empty?
+        puts "The line is currently empty."
+        return "The line is currently empty."
+    else 
+        str = ""
+        katz_deli.each_with_index{|name, index| str << " #{index +1}. #{name}"}
+        str.insert(0, "The line is currently:")
+    end 
+    puts str
+end
+
+#take_a_number method
+
+def take_a_number(katz_deli,name)
+    katz_deli.append("#{name}")
+    puts "Welcome, #{name}. You are number #{katz_deli.length} in line."  
+end
+
+# now_serving method 
+
+def now_serving(katz_deli)
+
+    if katz_deli.length > 1
+        puts "Currently serving #{katz_deli[0]}."
+        katz_deli.shift
+    else
+        puts "There is nobody waiting to be served!"
+    end
+
+end
